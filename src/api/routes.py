7bbd5edf6c54e249caafa62a3355aaba0b8cf087
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2024-07-26 23:23:55 -0400

    It works

--- src/api/routes.py
+++ src/api/routes.py
@@ -8,8 +8,11 @@ from aiohttp.web import Response
 
 from utils.cors import add_cors_routes
 from utils.limiter import Limiter
+from utils.authenticate import Approval, Key, authenticate, get_project_status
+from utils.emotion import detect_emotion
 
 if TYPE_CHECKING:
+  from utils.authenticate import User
   from utils.extra_request import Request
 
 with open("config.toml") as f:
@@ -21,6 +24,7 @@ with open("config.toml") as f:
 limiter = Limiter(exempt_ips=exempt_ips)
 routes = web.RouteTableDef()
 
+
 @routes.get("/srv/get/")
 @limiter.limit("60/m")
 async def get_lp_get(request: Request) -> Response:
@@ -30,10 +34,43 @@ async def get_lp_get(request: Request) -> Response:
   }
 
   if request.app.POSTGRES_ENABLED:
-    database_size_record = await request.conn.fetchrow("SELECT pg_size_pretty ( pg_database_size ( current_database() ) );")
-    packet["db_size"] = database_size_record.get("pg_size_pretty","-1 kB")
+    database_size_record = await request.conn.fetchrow(
+      "SELECT pg_size_pretty ( pg_database_size ( current_database() ) );"
+    )
+    packet["db_size"] = database_size_record.get("pg_size_pretty", "-1 kB")
 
   return web.json_response(packet)
+
+
+@routes.post("/emotion/")
+@limiter.limit("60/m")
+async def post_chat(request: Request) -> Response:
+  auth = await authenticate(request, cs=request.session)
+
+  if isinstance(auth, Response):
+    return auth
+  else:
+    if isinstance(auth, Key):
+      # this means its a Key
+      user: User = auth.user
+    else:
+      user: User = auth
+
+    status = await get_project_status(user, "emotion", cs=request.session)
+    if status != Approval.APPROVED:
+      return Response(
+        status=401,
+        text="please apply for project at https://auth.skystuff.cc/projects#emotion",
+      )
+
+  text = await request.text()
+  try:
+    response = await detect_emotion(text)
+  except Exception as e:
+    return Response(status=500, text=str(e))
+
+  return web.json_response(response)
+
 
 async def setup(app: web.Application) -> None:
   for route in routes:
---
+++ src/utils/emotion.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+import asyncio
+import tomllib
+from transformers import pipeline
+import torch
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+  pass
+
+with open("config.toml") as f:
+  config = tomllib.loads(f.read())
+
+
+MODEL_ID = config["ai"]["model"]
+DEVICE = config["ai"]["device"]
+MODEL_LOCK = asyncio.Lock()
+pipe = pipeline("text-classification", model=MODEL_ID, device=DEVICE)
+
+if not torch.cuda.is_available() and "cuda" in DEVICE:
+  raise Exception("Cuda is not available for model inference!")
+
+def get_output(text: str) -> dict:
+  result = pipe(text)
+  first = result[0]
+  return {"emotion": first["label"], "confidence": first["score"]}
+
+async def detect_emotion(text: str) -> dict:
+  loop = asyncio.get_running_loop()
+  async with MODEL_LOCK:
+    result = await loop.run_in_executor(None, get_output, text)
+  return result

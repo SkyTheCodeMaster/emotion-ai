3700af23e17e0d77800350e7e88ab50f3148d11c
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2024-08-09 12:14:47 -0400

    New classification method

--- src/utils/emotion.py
+++ src/utils/emotion.py
@@ -6,10 +6,10 @@ import tomllib
 from typing import TYPE_CHECKING
 
 import torch
-from transformers import pipeline
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
 if TYPE_CHECKING:
-  from transformers import TextClassificationPipeline
+  pass
 
 with open("config.toml") as f:
   config = tomllib.loads(f.read())
@@ -18,21 +18,27 @@ with open("config.toml") as f:
 MODEL_ID = config["ai"]["model"]
 DEVICE = config["ai"]["device"]
 MODEL_LOCK = asyncio.Lock()
-pipe: TextClassificationPipeline = pipeline("text-classification", model=MODEL_ID, device=DEVICE)
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
+model = AutoModelForSequenceClassification.from_pretrained(MODEL_ID)
 
 if not torch.cuda.is_available() and "cuda" in DEVICE:
   raise Exception("Cuda is not available for model inference!")
 
 
-def get_output(text: str) -> dict:
-  result = pipe(text, function_to_apply="none")
-  return result
+def classify(text):
+  inputs = tokenizer(text, return_tensors="pt")
+  outputs = model(**inputs)
+  probs = torch.nn.functional.softmax(outputs.logits, dim=-1)
+  return [
+    {"label": label, "score": prob.item()} for label, prob in enumerate(probs)
+  ]
 
 
 async def detect_emotion(text: str) -> dict:
   loop = asyncio.get_running_loop()
   async with MODEL_LOCK:
-    result = await loop.run_in_executor(None, get_output, text)
+    result = await loop.run_in_executor(None, classify, text)
   cleanup()
   return result
 
